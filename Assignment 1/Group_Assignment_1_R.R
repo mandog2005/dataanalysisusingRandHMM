d1a5d96a6ae5e7d45310bedf28d0a75d72e811ef
ahalden <ali_hasan_alden@sfu.ca>
2021-10-19 14:31:21 -0700

    finished product with git information and assignment1 data files

--- Assignment 1/Group_Assignment_1_R.R
+++ Assignment 1/Group_Assignment_1_R.R
@@ -1,16 +1,38 @@
 #how the system should work and you can check anomaly on the system itself
 #What is the point? : trying to learn a normal behaviour of the system.
-
 library(lubridate)
+library(tidyverse)
 
+ggplotRegression <- function (fit) {
+  
+  require(ggplot2)
+  
+  ggplot(fit$model, aes_string(x = names(fit$model)[2], y = names(fit$model)[1])) + 
+    geom_point() +
+    stat_smooth(method = "lm", col = "red") +
+    labs(title = paste("Adj R2 = ",signif(summary(fit)$adj.r.squared, 5),
+                       "Intercept =",signif(fit$coef[[1]],5 ),
+                       " Slope =",signif(fit$coef[[2]], 5),
+                       " P =",signif(summary(fit)$coef[2,4], 5)))
+}
+
+library(dplyr)
 getwd()
 setwd("C:/Users/alial/OneDrive/Desktop/318_ASSIGNMENTS_REPOSITORY/CMPT318_FALL2021/Assignment 1")
 df <- read.table("Group_Assignment_1_Dataset.txt", header = TRUE, sep = ",")
+dfMorning <- read.table("Group_Assignment_1_Dataset.txt", header = TRUE, sep = ",")
 dates <- dmy(df$Date)
 df$Date<- as.Date(dates)
+df$Time <- as_datetime(df$Date + hms(df$Time))
+
+
 
 df <- df[df$Date >= "2007-03-05" & df$Date <= "2007-03-11",]
 remove(dates)
+dfMorning <- df[hour(df$Time) >= 8 & hour(df$Time) <= 16,]
+dfNight <- df[hour(df$Time) >= 17 & hour(df$Time) <= 23,]
+dfdata <- read.table("data1.csv", header = TRUE, sep = ";")
+
 library(ggplot2)
 # function to get the mode.
 getmode <- function(v, na.rm = FALSE) {
@@ -50,26 +72,26 @@ getmode <- function(v, na.rm = FALSE) {
 #TASK 1 part A week 10:
 
 #function for computing Arithmetic mean for part A week 10 week 10
-avgActivePowerArithMeanA <- mean(df$Global_active_power, na.rm = TRUE)
+dfdata[1,1] <- mean(df$Global_active_power, na.rm = TRUE)
 print(avgActivePowerArithMeanA)
 
 #function for computing Geometric mean for part A week 10 week 10
-avgActivePowerGeometricMeanA <- exp(mean(log(df$Global_active_power), na.rm=TRUE))
+dfdata[1,2] <- exp(mean(log(df$Global_active_power), na.rm=TRUE))
 print(avgActivePowerGeometricMeanA)
 
 
 #function for computing median for part A week 10
 #na.rm = TRUE just means we ignore null values in this case.
-avgActivePowerMedianA <- median(df$Global_active_power , na.rm = TRUE)
+dfdata[1,3] <- median(df$Global_active_power , na.rm = TRUE)
 print(avgActivePowerMedianA)
 
 
 #function for computing mode for part A week 10
-avgActivePowerModeA <- getmode(df$Global_active_power)
+dfdata[1,4] <- getmode(df$Global_active_power)
 print(avgActivePowerModeA)
 
 #function for computing std for part A week 10
-StdActivePowerA <- sd(df$Global_active_power , na.rm = TRUE)
+dfdata[1,5] <- sd(df$Global_active_power , na.rm = TRUE)
 print(StdActivePowerA)
 
 #Task A done
@@ -83,23 +105,23 @@ print(StdActivePowerA)
 #TASK 1 part B week 10:
 
 #function for computing Arithmetic mean for part B week 10
-avgReactivePowerArithMeanB <- mean(df$Global_reactive_power , na.rm = TRUE)
+dfdata[2,1] <- mean(df$Global_reactive_power , na.rm = TRUE)
 print(avgReactivePowerArithMeanB)
 
 #function for computing Geometric mean for part B week 10
-avgReactivePowerGeometricMeanB <- exp(mean(log(df$Global_reactive_power)& df$Date >= "2007-03-05" & df$Date <= "2007-03-11",na.rm = TRUE))
+dfdata[2,2] <- exp(mean(log(df$Global_reactive_power)& df$Date >= "2007-03-05" & df$Date <= "2007-03-11",na.rm = TRUE))
 print(avgReactivePowerGeometricMeanB)
 
 #function for computing median for part B week 10
-avgReactivePowerMedianB <- median(df$Global_reactive_power& df$Date >= "2007-03-05" & df$Date <= "2007-03-11", na.rm = TRUE)
+dfdata[2,3] <- median(df$Global_reactive_power& df$Date >= "2007-03-05" & df$Date <= "2007-03-11", na.rm = TRUE)
 print(avgReactivePowerMedianB)
 
 #function for computing mode for part B week 10
-avgReactivePowerModeB <- getmode(df$Global_reactive_power,na.rm = TRUE)
+dfdata[2,4] <- getmode(df$Global_reactive_power,na.rm = TRUE)
 print(avgReactivePowerModeB)
 
 #function for computing std for part B week 10
-StdReactivePowerB <- sd(df$Global_reactive_power& df$Date >= "2007-03-05" & df$Date <= "2007-03-11",na.rm = TRUE)
+dfdata[2,5] <- sd(df$Global_reactive_power& df$Date >= "2007-03-05" & df$Date <= "2007-03-11",na.rm = TRUE)
 print(StdReactivePowerB)
 
 
@@ -112,23 +134,19 @@ print(StdReactivePowerB)
 
 
 #function for computing Arithmetic mean for part C week 10
-avgVoltageArithMeanC <- mean(df$Voltage, na.rm = TRUE)
-print(avgVoltageArithMeanC)#checking value
+dfdata[3,1] <- mean(df$Voltage, na.rm = TRUE)
 
 #function for computing Geometric mean for part C week 10
-avgVoltageGeoMeanC <- exp(mean(log(df$Voltage) ,na.rm = TRUE))
-print(avgVoltageGeoMeanC) #checking value
+dfdata[3,2] <- exp(mean(log(df$Voltage) ,na.rm = TRUE))
 
 #function for computing median for part C week 10
-avgVoltageMedianC <- median(df$Voltage,na.rm = TRUE)
-print(avgVoltageMedianC) #checking value
+dfdata[3,3] <- median(df$Voltage,na.rm = TRUE)
 
 #function for computing mode for part C week 10
-avgVoltageModeC <- getmode(df$Voltage, na.rm = TRUE)
+dfdata[3,4] <- getmode(df$Voltage, na.rm = TRUE)
 
 #function for computing std for part C week 10
-stdVoltageC <- sd(df$Voltage,na.rm = TRUE)
-print(stdVoltageC)
+dfdata[3,5] <- sd(df$Voltage,na.rm = TRUE)
 
 #Task C Done
 #TASK 1 part C week 10:
@@ -142,7 +160,7 @@ print(stdVoltageC)
 #max value for active power between monday to friday
 maxValueWeekdayActive <- max(df[,3],na.rm = TRUE, wday(df[,1]) > 0 & wday(df[,1]) < 6)
 #min value for active power between monday to friday
-minValueWeekdayActive <- min(df[,3],na.rm = TRUE, wday(df[,1]) > 0 & wday(df[,1]) < 6)
+minValueWeekdayActive <- min(df$Global_active_power,na.rm = TRUE,df$Global_active_power!=0 & wday(df[,1]) > 0 & wday(df[,1]) < 6)
 
 #max value for Reactive power between monday to friday
 maxValueWeekdayReactive <- max(df[,4],na.rm = TRUE, wday(df[,1]) > 0 & wday(df[,1]) < 6)
@@ -197,11 +215,15 @@ minValueWeekdayReactive <- min(df[,4],na.rm = TRUE, wday(df[,1]) > 0 & wday(df[,
 
 #Correlation of Reactive to Active Power
 CorrAB <- cor(df$Global_active_power,df$Global_reactive_power,use="complete.obs",method="pearson")
-#Correlation of Reactive to 
-CorrAB <- cor(df$Global_active_power,df$Global_reactive_power,use="complete.obs",method="pearson")
-#Correlation of Reactive to Active Power
-CorrAB <- cor(df$Global_active_power,df$Global_reactive_power,use="complete.obs",method="pearson")
+#Correlation of Reactive to Voltage
+CorrAC <- cor(df$Global_active_power,df$Global_reactive_power,use="complete.obs",method="pearson")
+#Correlation of Reactive to intensity
+CorrAD <- cor(df$Global_active_power,df$Global_reactive_power,use="complete.obs",method="pearson")
 
+CorrBC <- cor(df$Global_reactive_power,df$Voltage,use="complete.obs",method="pearson")
+CorrBD <- cor(df$Global_reactive_power,df$Global_intensity,use="complete.obs",method="pearson")
+
+CorrCD <- cor(df$Voltage,df$Global_intensity,use="complete.obs",method="pearson")
 
 #active power in kilowatt given that kitchen is used
 CorrAE <- cor(df$Global_active_power,df$Sub_metering_1,use="complete.obs",method="pearson")
@@ -226,8 +248,47 @@ CorrFG <- cor(df$Sub_metering_2,df$Sub_metering_3,use="complete.obs",method="pea
 
 
 ggplot()+
-  layer(data = df, mapping = aes(x=Time, y=Global_active_power), geom = "point",stat="identity", position = position_identity())
+  layer(data = df, mapping = aes(x=Time, y=Global_active_power) ,geom = "point",stat="identity", position = position_identity())
+
+ggplot()+
+  layer(data = df, mapping = aes(x=Time, y=Global_reactive_power), geom = "point",stat="identity", position = position_identity())
 
 ggplot()+
   layer(data = df, mapping = aes(x=Time, y=Voltage), geom = "point",stat="identity", position = position_identity())
 
+#Highly correlated values
+ggplot()+
+  layer(data = df, mapping = aes(x=Global_reactive_power, y=Global_intensity), geom = "point",stat="identity", position = position_identity())
+
+#weakly-strong correlated values corr .50 < i < .70
+
+ggplot()+
+  layer(data = df, mapping = aes(x=Sub_metering_1, y=Voltage), geom = "point",stat="identity", position = position_identity())
+
+
+
+
+########################################################################
+
+ggplot(data = df, aes(Time, Global_intensity)) +
+  geom_point()
+
+dfMorning %>%
+  tail(150) %>%
+  ggplot( aes(x=Time, y=Global_intensity)) +
+  geom_line() +
+  geom_point()
+
+dfNight %>%
+  tail(150) %>%
+  ggplot( aes(x=Time, y=Global_intensity)) +
+  geom_line() +
+  geom_point()
+
+
+fit_linearMorning <- lm(Global_intensity~Time,dfMorning)
+fit_linearNight <- lm(Global_intensity~Time,dfNight)
+fit_polynomial <- lm(Global_intensity ~ poly(y, 2, raw=TRUE) ,dfMorning)
+
+  
+write.csv2(dfdata, file = "data1.csv")
